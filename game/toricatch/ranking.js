d26162594ac6e43c3c185d7e64b92023e463c2bd
pippi1218 <ma7pippi@gmail.com>
2021-09-12 12:42:22 +0900

    add name and point to DB

--- game/toricatch/ranking.js
+++ game/toricatch/ranking.js
@@ -1,16 +1,19 @@
-var db = firebase.database();
-
+var db = firebase.firestore();
 
 function submit() {
     var el_n = document.getElementById("e_name")
     var name = el_n.value
-    if (name == "") {
+
+    var el_p = document.getElementById("e_point")
+    var point = Number(el_p.innerText)
+
+    if (name == "" || point == "") {
         alert("Please fill out the blank!");
         return;
     }
     db.collection("scores").add({
         name: name,
-        point: 12,
+        point: point,
     })
     .then(() => {
         console.log("Document successfully written!");
