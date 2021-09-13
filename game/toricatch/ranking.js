cf04429546b00338f741301e620329f45ceec1a5
pippi1218 <ma7pippi@gmail.com>
2021-09-14 01:13:56 +0900

    firestore からの読み取りコード追加

--- game/toricatch/ranking.js
+++ game/toricatch/ranking.js
@@ -1,11 +1,12 @@
-var db = firebase.firestore();
-
 function submit() {
+
     var el_n = document.getElementById("e_name")
     var name = el_n.value
 
-    var el_p = document.getElementById("e_point")
-    var point = Number(el_p.innerText)
+    //var el_p = document.getElementById("e_point")
+    //var point = Number(el_p.innerText)
+
+    var point = 100
 
     if (name == "" || point == "") {
         alert("Please fill out the blank!");
@@ -21,4 +22,31 @@ function submit() {
     .catch((error) => {
         console.error("Error writing document: ", error);
     });
+}
+
+function read_ranking(){
+    var LIST = [];  //ID保管用
+
+    db.collection("scores").get().then((querySnapshot) => {
+            var buff = [];
+            var html = "<ul>";
+            querySnapshot.forEach((doc) => {
+                var data = doc.data();
+                html += `<li>${data.name}, ${data.point}</li>`;
+                buff.push(doc.id);
+            });
+            html += "</ul>";
+            LIST = buff;
+            showMessage(html);
+        })
+        .catch((error)=>{
+            showMessage(`データの取得に失敗しました (${error})`);
+        });
+
+
+    function showMessage(str){
+        var msg = document.querySelector("#ranking");
+        msg.innerHTML = str;
+        msg.style.display = "block";
+    }
 }
